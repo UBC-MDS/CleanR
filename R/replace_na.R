aa34947fa2623104d00cdf2e4d5e11b8edf36f4c
Phuntsok Tseten <phuntsok04@gmail.com>
2019-03-01 08:16:04 -0800

    added replace_na function

--- R/replace_na.R
+++ R/replace_na.R
@@ -10,9 +10,48 @@
 #' @return
 #' A list of tuples where each NAs will be replaced by either
 #' min, max, median or average.
-#' 
+#'
 #' @export
 
 replace_na <- function(data) {
 
+  if (!any(is.na(data))) {
+    return ("Input must not all me missing values")
+
+
+      # mean
+  mean_na = list()
+      for (i in 1:ncol(data)) {
+        data[is.na(data[,i]), i] <- mean(data[,1], na.rm = TRUE)
+      }
+      return(mean_na)
+    }
+
+    # min
+  min_na = list()
+    for (i in 1:ncol(data)) {
+      data[is.na(data[,i]), i] <- mean(data[,1], na.rm = TRUE)
+    }
+    return(min_na)
+  }
+
+  #  median
+
+  median_na = list()
+    for (i in 1:ncol(data)) {
+    data[is.na(data[,i]), i] <- with(
+      median(data[,2], na.rm = TRUE))
+  }
+  return(median_na)
 }
+
+# max
+
+ max_na = list()
+   for (i in 1:ncol(data)) {
+  data[is.na(data[,i]), i] <- with(
+    max(data[,2], na.rm = TRUE))
+}
+return(max_na)
+}
+
