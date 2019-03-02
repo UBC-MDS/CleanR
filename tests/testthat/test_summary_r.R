5153901727e20ef6ef11dd0f98e63ed9334d9622
Heather Van Tassel <heathervt12@gmail.com>
2019-03-02 14:55:24 -0800

    summary tests added

--- tests/testthat/test_summary_r.R
+++ tests/testthat/test_summary_r.R
@@ -3,30 +3,44 @@ context("Replace NA function")
 library(CleanR)
 
 toy_data_tbl <- tribble(
-  ~x, ~y,  ~z,
-  NA, 2,  3.6,
-  "b", NA, 8.5,
-  "c", NA, NA
+  ~x, ~y, 
+  NA, 2,  
+  "b", 1.5,
+  "c", NA
 )
 toy_data_df <- as.data.frame(toy_data_tbl)
 toy_all_na <- tibble(x = c(NA,NA,NA), y = c(NA,NA,NA), z = c(NA, NA, NA))
 toy_no_na <- tibble(x = c(1,2,3,4), y = c(5,6,7,8))
-toy_same_dim <- tibble(x = c(3,4), y = c(5,6))
-
+toy_categorical <- tibble(x= c(1,2))
 test_that("Test that input is a data frame", {
-  expect_error(summary("Input Data"), "Input is not a data frame")
-})
-
-test_that("Test that input has NA", {
-  all_na_result <- list(x=c(1,2,3), y=c(1,2,3), z=c(1,2,3))
-
-  expect_warning(summary(toy_all_na), "There are no missing values")
-
+  expect_error(summary_r("Input Data"), "Input is not a data frame")
 })
 
 
-test_that("Test that the input dimension equals the output dimension", {
-  same_dim_result <- list(x=c(0), y=c(0))
+test_that("Dimensions are correct for a regular dataframe", {
+  expect_error(summary_r("Input Data"), "Summary is not implemented on objects with more than 2 dimensions")
+})
 
-  expect_warning(summary(toy_same_dim), "The dimensions don't match")
+test_that("Test that the dataframe is not empty", {
+  empty_df <- data.frame( )
+  expect_error(summary_r(empty_df), "Cannot summarize a dataframe without observations")
+    })
+
+test_that("Test that the results for numerical and categorical data make sense", {
+  toy_data_results <- list("x"= list("count"       =   3,
+                                     "count_unique"=   3,
+                                     "unique"      =   NA, "b", "c", 
+                                     "count_NAs"   =   1),
+                           "y" = list("count"       =   3,
+                                      "count_unique"=   3,
+                                      "unique"      =   2.0, 1.5, NA,
+                                      "count_NAs"   =   3,
+                                      "min_"        =   1.5,
+                                      "max_"        =   2,
+                                      "mean"        =   1.75,
+                                      "median"      =   1.75))
+                                
+                         
+                       
+  expect_equal(summary_r(toy_data_df), toy_data_results)
 })
